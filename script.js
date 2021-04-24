32be85cc82eb60f794c92aa44a28336f984803ae
Bedkow <bednarekdaniel@o2.pl>
2021-04-24 14:12:47 +0200

    getting game info from API, planning

--- script.js
+++ script.js
@@ -35,6 +35,53 @@ bottom: stores that sell the game - https://api.rawg.io/api/games/{game_pk}/stor
 
 // calling RAWG using promises like a civilised dev
 
-fetch('https://api.rawg.io/api/games?key=bcfe18881b8345c89b572cdb49c10987&search_precise=true&search="valheim"')
-.then(response => response.json())
-.then(data => console.log(data));
+fetch(
+	'https://api.rawg.io/api/games?key=bcfe18881b8345c89b572cdb49c10987&search_precise=true&search="valheim"'
+)
+	.then((response) => response.json())
+	.then((data) => console.log(data));
+
+const btn = document.querySelector(".input_btn");
+// const art = document.querySelector('.game_art');
+// const info = document.querySelector('.info_container');
+// const screenshots = document.querySelector('.screenshots_container');
+// const stores = document.querySelector('.stores');
+const all_info = document.querySelector(".game_info");
+
+const renderGame = function (data) {
+	const html = `
+    <img class="game_art" src=${data.results[0].background_image}></div>
+    <div class="info_container">
+    <div>Name: ${data.results[0].name}</div>
+    <div>Released: ${data.results[0].released}</div>
+    <div>Playtime?</div>
+    <div>Twitch count?</div>
+    <div>YT count?</div>
+    <div>Platforms:</div>
+    <div>Requirements:</div>
+    <div>Metacritic link:</div>
+    <div>Screenshots:</div>
+    <div>Stores:</div>
+    <div></div>
+    </div>
+    <div class="screenshots_container"></div>
+    <div class="stores"></div>
+    `;
+
+	all_info.insertAdjacentHTML("beforeend", html);
+};
+
+const getGameData = function (game) {
+	fetch(
+		`https://api.rawg.io/api/games?key=bcfe18881b8345c89b572cdb49c10987&search_precise=true&search="${game}"`
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			renderGame(data);
+		})
+		.catch((err) => console.log(`error: ${err}`));
+};
+
+btn.addEventListener("click", function () {
+	getGameData("valheim");
+});
