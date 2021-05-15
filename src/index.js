32ba40899c0522e4a382fa012835c4aee271aeda
Bedkow <bednarekdaniel@o2.pl>
2021-05-15 17:03:10 +0200

    webpack ready for action, time to proceed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,64 @@
-//import dayjs from "dayjs";
-// import {relativeTime} from ".dayjs/plugin/relativeTime"
-//dayjs.extend(relativeTime)
-import "./main.scss"
-import {app} from "./app/app";
-// import dayjs from "dayjs";
-app();
+/*
+details of the game - 
+1: game art
+2: info - name, released, playtime, twitch count, youtube count, platforms > requirements, metacritic link
+
+3: screenshots
+
+bottom: stores that sell the game - https://api.rawg.io/api/games/{game_pk}/stores
+
+*/
+
+// calling RAWG using promises
+// fetch(
+// 	'https://api.rawg.io/api/games?key=bcfe18881b8345c89b572cdb49c10987&search_precise=true&search="valheim"'
+// )
+// 	.then((response) => response.json())
+// 	.then((data) => console.log(data));
+
+const btn = document.querySelector(".input_btn");
+const all_info = document.querySelector(".game_info");
+
+const renderGame = function (data) {
+	//get current time
+	let [currentMonth, currentDate, currentYear] = new Date()
+		.toLocaleDateString("en-US")
+		.split("/");
+
+	// //get release time
+
+	let releaseTime = data.results[0].released;
+	let [releaseYear, releaseMonth, releaseDay] = releaseTime.split("-");
+
+	// get total time passed
+
+	// let totalTimePassed = dayjs(
+	// 	`${currentYear}-${currentMonth}-${currentDate}`
+	// ).fromNow();
+
+	const html = `
+    <div class="imgtitle">
+    <img class="game_art" src=${data.results[0].background_image}>
+    <div class="art_title">${data.results[0].name}</div>
+    </div>
+    <div class="info_container">
+    <div>Released: ${data.results[0].released} <br></div>
+ `;
+
+	all_info.insertAdjacentHTML("beforeend", html);
+};
+
+const getGameData = function (game) {
+	fetch(
+		`https://api.rawg.io/api/games?key=3a4e64a027444e258be25283e5bd967a&search_precise=true&search="${game}"`
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			renderGame(data);
+		})
+		.catch((err) => console.log(`error: ${err}`));
+};
+
+btn.addEventListener("click", function () {
+	getGameData("valheim");
+});
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const { styles } = require('ansi-colors');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    mode: 'none',
+    devServer: {
+        contentBase: path.join(__dirname,'./dist'),
+        port: 9000,
+        watchContentBase: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/env' ]
+                    }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new TerserPlugin(), 
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+    ]
+}
