c1fee456c13d3a21e7209c3606adc740cf18a1e3
Bedkow <bednarekdaniel@o2.pl>
2021-05-20 23:51:12 +0200

    progress with img srces

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
 /*
@@ -18,65 +18,105 @@ bottom: stores that sell the game - https://api.rawg.io/api/games/{game_pk}/stor
 
 const btn = document.querySelector(".input_btn");
 const all_info = document.querySelector(".game_info");
-const inputForm = document.querySelector('#inputForm');
-const gameName = document.querySelector('#gameName')
-
-// get game info by id
+const inputForm = document.querySelector("#inputForm");
+const gameName = document.querySelector("#gameName");
+const screenshotsContainer = document.querySelector(".screenshots");
 
 // action on click -> getting name and putting it into getGameData function
-
 inputForm.addEventListener("submit", function () {
 	all_info.innerHTML = ``;
-	let gameName = document.querySelector('#gameName').value;
+	let gameName = document.querySelector("#gameName").value;
 	getGameData(gameName);
+	//call func to insert screenshots
+	insertScreenshots(gameID);
 });
 
-
 // fetching game's data, putting it into renderGame function
-
 const getGameData = function (game) {
 	fetch(
 		`https://api.rawg.io/api/games?key=3a4e64a027444e258be25283e5bd967a&search_precise=true&search="${game}"`
 	)
 		.then((response) => response.json())
 		.then((data) => {
-			renderGame(data); 
+			renderGame(data);
 		})
 
 		.catch((err) => console.log(`error: ${err}`));
 };
 
+// insert screenshots into html func
+const insertScreenshots = function (gameID) {
+	fetch(
+		`https://api.rawg.io/api/games/${gameID}/screenshots?key=3a4e64a027444e258be25283e5bd967a`
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data); ////
+			let screenshotURL = [];
+			for (let i = 0; i <= data.results.length && i <= 4; i++) {
+				console.log(i); ///////////
+
+				screenshotURL.push(data.results[i].image);
+				let srcImgUrl = `<img src="${screenshotURL[i]}></img>"`;
+				document
+					.querySelector(".screenshots")
+					.insertAdjacentHTML("beforeend", srcImgUrl);
+			}
+
+			console.log(screenshotURL); ////
+		});
+};
 
 // rendering html with fetched data
 
 const renderGame = function (data) {
-
-	console.log(data.results[0].id)
+	console.log(data.results[0].id); // temp for DEV !!!
 
 	//fetching by ID test
-	fetch(`https://api.rawg.io/api/games/${data.results[0].id}?key=3a4e64a027444e258be25283e5bd967a`)
-		.then(response => response.json())
-		.then(data => {
-			console.log(data);
-		})
+	fetch(
+		`https://api.rawg.io/api/games/${data.results[0].id}?key=3a4e64a027444e258be25283e5bd967a`
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			console.log(data); // temp for DEV !!!
 
-	// get release time
+			let releaseTime = data.released;
 
-	let releaseTime = data.results[0].released;
-	let [releaseYear, releaseMonth, releaseDay] = releaseTime.split("-");
+			// render html with data and insert it into the DOM
+			let html = `
+				<div class="imgtitle">
 
-	// render html with data and insert it into the DOM
+				<img class="game_art" src=${data.background_image}>
 
-	let html = `
-    <div class="imgtitle">
-    <img class="game_art" src=${data.results[0].background_image}>
-    <div class="art_title">${data.results[0].name}</div>
-    </div>
-    <div class="info_container">
-    <div>Released: ${data.results[0].released} (${dayjs().to(dayjs(releaseTime))})<br></div>
-	<div>Dev_ID : ${data.results[0].id}
-	<br></div>
- `;
+				<div class="art_title">${data.name}</div>
+				</div>
 
-	all_info.insertAdjacentHTML("beforeend", html);
+				<div class="info_container">
+				<div class="released">Released: ${data.released} (${dayjs().to(
+				dayjs(releaseTime)
+			)})</div><br>
+
+				<div class="description"> ${data.description}
+				</div><br>
+
+				<div class="metacritic"> Metacritic score: ${data.metacritic}
+				 <!-- fix: undefined when empty!!!!!!!!!!!!!!!!! -->
+				</div><br>
+
+				<div class="platforms"> Platforms:
+				</div><br>
+
+				<div class="pcRequirements"> Requirements:<br> Minimum: xxx <br>
+				Recommended: xxx <br>
+				</div><br>
+
+				<div class="screenshots">
+				</div><br>
+			`;
+
+			all_info.insertAdjacentHTML("beforeend", html);
+			let gameID = data.id;
+			return gameID;
+		});
+	return gameID;
 };
