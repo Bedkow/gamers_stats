a7ac02da348f207c29744be0a1b5d3c69e22b3ea
Bedkow <bednarekdaniel@o2.pl>
2021-05-30 00:28:59 +0200

    after required lint config and lint errors fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "./index.scss";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
+import './index.scss';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 
 // API requires 1 fetch before responding for some reason
@@ -20,103 +20,102 @@ bottom: stores that sell the game - https://api.rawg.io/api/games/{game_pk}/stor
 
 */
 
-const btn = document.querySelector(".input_btn");
-const all_info = document.querySelector(".game_info");
-const inputForm = document.querySelector("#inputForm");
-const gameName = document.querySelector("#gameName");
-
-// action on click -> getting name and putting it into getGameData function
-inputForm.addEventListener("submit", function () {
-	all_info.innerHTML = ``;
-	let gameName = document.querySelector("#gameName").value;
-	getGameData(gameName).then((data) => renderGame(data));
-});
+const btn = document.querySelector('.input-btn');
+const allInfo = document.querySelector('.game-info');
+const inputForm = document.querySelector('.input-form');
 
 // fetching game's data, putting it into renderGame function
 const getGameData = function (game) {
-	return fetch(
-		`https://api.rawg.io/api/games?key=3a4e64a027444e258be25283e5bd967a&search_precise=true&search="${game}"`
-	)
-		.then((response) => response.json())
-		.then((data) => {
-			return data;
-		})
+  return fetch(
+    `https://api.rawg.io/api/games?key=3a4e64a027444e258be25283e5bd967a&search_precise=true&search="${game}"`,
+  )
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
 
-		.catch((err) => console.log(`error: ${err}`));
+    .catch(err => console.log(`error: ${err}`));
 };
 
 // rendering html with fetched data
 
 const renderGame = function (data) {
-	console.log(data.results[0].id); // temp for DEV !!!
-	let gameID = data.results[0].id;
-	//fetching by ID test
-	fetch(
-		`https://api.rawg.io/api/games/${data.results[0].id}?key=3a4e64a027444e258be25283e5bd967a`
-	)
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data); // temp for DEV !!!
+  console.log(data.results[0].id); // temp for DEV !!!
+  const gameID = data.results[0].id;
+  //fetching by ID test
+  fetch(
+    `https://api.rawg.io/api/games/${data.results[0].id}?key=3a4e64a027444e258be25283e5bd967a`,
+  )
+    .then(response => response.json())
+    .then(data => {
+      console.log(data); // temp for DEV !!!
 
-			let releaseTime;
-			if (data.tba === true) {
-				releaseTime = "TBA";
-			} else {
-				releaseTime = `${data.released} (${dayjs().to(dayjs(data.released))})`;
-			}
+      let releaseTime;
+      if (data.tba === true) {
+        releaseTime = 'TBA';
+      } else {
+        releaseTime = `${data.released} (${dayjs().to(dayjs(data.released))})`;
+      }
 
-			let metacriticScore;
-			if (data.metacritic === null) {
-				metacriticScore = "Game not found on Metacritic";
-			} else {
-				metacriticScore = data.metacritic;
-			}
+      let metacriticScore;
+      if (data.metacritic === null) {
+        metacriticScore = 'Game not found on Metacritic';
+      } else {
+        metacriticScore = data.metacritic;
+      }
 
-			// render html with data and insert it into the DOM
-			let html = `
-				<div class="imgtitle">
+      // render html with data and insert it into the DOM
+      const html = `
+				  <div class="imgtitle">
+  
+				  <img class="game-art" src=${data.background_image}>
+  
+				  <div class="art-title">${data.name}</div>
+				  </div>
+  
+				  <div class="info-container">
+				  <div class="released">Released: ${releaseTime}</div><br>
+  
+				  <div class="description"> ${data.description}
+				  </div><br>
+  
+				  <div class="metacritic"> Metacritic score: ${metacriticScore}
+				  </div><br>
+  
+				  <div class="platforms"> Platforms:
+				  </div><br>
+  
+				  <div class="pc-requirements"> Requirements:<br> Minimum: xxx <br>
+				  Recommended: xxx <br>
+				  </div><br>
+  
+				  <div class="screenshots">
+				  </div><br>
+			  `;
 
-				<img class="game_art" src=${data.background_image}>
+      allInfo.insertAdjacentHTML('beforeend', html);
 
-				<div class="art_title">${data.name}</div>
-				</div>
-
-				<div class="info_container">
-				<div class="released">Released: ${releaseTime}</div><br>
-
-				<div class="description"> ${data.description}
-				</div><br>
-
-				<div class="metacritic"> Metacritic score: ${metacriticScore}
-				</div><br>
-
-				<div class="platforms"> Platforms:
-				</div><br>
-
-				<div class="pcRequirements"> Requirements:<br> Minimum: xxx <br>
-				Recommended: xxx <br>
-				</div><br>
-
-				<div class="screenshots">
-				</div><br>
-			`;
-
-			all_info.insertAdjacentHTML("beforeend", html);
-
-			// insert screenshots into html
-			return fetch(
-				`https://api.rawg.io/api/games/${gameID}/screenshots?key=3a4e64a027444e258be25283e5bd967a`
-			)
-				.then((response) => response.json())
-				.then((data) => {
-					console.log(data); ////
-					let image = [];
-					for (let i = 0; i <= data.results.length && i <= 4; i++) {
-						console.log(i); ///////////
-						image[i] = document.createElement("img");
-						image[i].src = data.results[i].image;
-						document.querySelector(".screenshots").appendChild(image[i]);
-					}
-				});
-		});
+      // insert screenshots into html
+      return fetch(
+        `https://api.rawg.io/api/games/${gameID}/screenshots?key=3a4e64a027444e258be25283e5bd967a`,
+      )
+        .then(response => response.json())
+        .then(data => {
+          console.log(data); ////
+          const image = [];
+          for (let i = 0; i <= data.results.length && i <= 4; i++) {
+            console.log(i); ///////////
+            image[i] = document.createElement('img');
+            image[i].src = data.results[i].image;
+            document.querySelector('.screenshots').appendChild(image[i]);
+          }
+        });
+    });
 };
+
+// action on click -> getting name and putting it into getGameData function
+inputForm.addEventListener('submit', function () {
+  allInfo.innerHTML = ``;
+  const gameName = document.querySelector('.game-name').value;
+  getGameData(gameName).then(data => renderGame(data));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,55 +3,63 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const dayjs = require('dayjs');
-// const relativeTime = require('dayjs/plugin/relativeTime');
-// dayjs.extend(relativeTime);
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
+const StylelintWebpackPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist'),
-    },
-    mode: 'none',
-    devServer: {
-        contentBase: path.join(__dirname,'./dist'),
-        port: 9000,
-        watchContentBase: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                    }
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [ '@babel/env' ]
-                    }
-                }
-            },
-        ]
-    },
-    plugins: [
-        new TerserPlugin(), 
-        new MiniCssExtractPlugin({
-            filename: 'styles.css',
-        }),
-    ]
-}
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  mode: 'none',
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    port: 9000,
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new TerserPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+    new ESLintWebpackPlugin({
+      failOnError: false,
+      failOnWarning: false,
+      formatter: 'codeframe',
+    }),
+    new StylelintWebpackPlugin({
+      files: './src/*.scss',
+      failOnError: false,
+      failOnWarning: false,
+      formatter: 'string',
+    }),
+  ],
+};
